ea540b3b026c6ab1ff57c0ea2988597f590dcb97
wmadwand <wmadwand@gmail.com>
2018-10-17 16:00:10 +0300

    Example01

---
+++ Example01.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Strategy
+{
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class PetrolMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на бензине");
+        }
+    }
+
+    class ElectricMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на электричестве");
+        }
+    }
+
+    class Car
+    {
+        protected int passengers; // кол-во пассажиров
+        protected string model; // модель автомобиля
+
+        public IMovable Movable { private get; set; }
+
+        public Car(int num, string model, IMovable mov)
+        {
+            this.passengers = num;
+            this.model = model;
+            Movable = mov;
+        }
+
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car(4, "Volvo", new PetrolMove());
+            auto.Move();
+            auto.Movable = new ElectricMove();
+            auto.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
