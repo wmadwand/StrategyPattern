85a163da69ebba13c13a8750c0593de7ee44b04f
wmadwand <wmadwand@gmail.com>
2018-11-28 11:23:38 +0300

    Recall

---
+++ Recall/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Recall
+{
+    public interface IEngine
+    {
+        void Use();
+    }
+
+    public class Gasoline : IEngine
+    {
+        public void Use()
+        {
+            Console.WriteLine("PetrolMove");
+        }
+    }
+
+    public class ElectroMove : IEngine
+    {
+        public void Use()
+        {
+            Console.WriteLine("ElectroMove");
+        }
+    }
+
+    public class Car
+    {
+        public IEngine engine;
+
+        public void Move()
+        {
+            engine.Use();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            IEngine gasolineEngine = new Gasoline();
+            IEngine electroEngine = new ElectroMove();
+
+            car.engine = gasolineEngine;
+
+            car.engine = electroEngine;
+        }
+    }
+}
